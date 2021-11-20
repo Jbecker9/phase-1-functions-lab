f830f023af6e3ae9dee9ecd7c220cf5b0a04f034
Jared <jbeckersalax9@aol.com>
2021-11-20 18:04:24 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 function distanceFromHqInBlocks(block){
-    const start = 42
-    let distance = Math.abs(block - start)
+    const hq = 42
+    let distance = Math.abs(block - hq)
         return distance 
 }
 
-function distanceFromHqinFeet(feet){
-    distanceFromHqInBlocks(block);
-    const length = 264
-    return distance = distanceFromHqInBlocks(block) * length
+function distanceFromHqInFeet(block){
+    let distance = distanceFromHqInBlocks(block);
+    const length = 264;
+    let feet = distance * length;
+    return feet;
 }
+
+function distanceTravelledInFeet(block, end){
+    const length = 264;
+    let travelled = Math.abs(block - end);
+    let feet = travelled * length;
+    return feet
+}
+
+function calculatesFarePrice(block, end){
+    let total = distanceTravelledInFeet(block, end);
+    let charge = .02
+        if (total <= 400){
+            let price = 0
+            return price
+        }
+        if (total > 400 && total <= 200)
+            return cost = (total - 400) * charge
+    }

38012189d8be3c0002f4a7955842a0e9325c35ba
Jared <jbeckersalax9@aol.com>
2021-11-20 16:15:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block){
+    const start = 42
+    let distance = Math.abs(block - start)
+        return distance 
+}
+
+function distanceFromHqinFeet(feet){
+    distanceFromHqInBlocks(block);
+    const length = 264
+    return distance = distanceFromHqInBlocks(block) * length
+}

f7d0aa8829bb67c42f8b96a02d9454844accdef1
Jared <jbeckersalax9@aol.com>
2021-11-20 18:27:44 -0500

    Done.

--- index.js
+++ index.js
@@ -21,12 +21,23 @@ function distanceTravelledInFeet(block, end){
 function calculatesFarePrice(block, end){
     let total = distanceTravelledInFeet(block, end);
     let charge = .02
+    console.log(total)
         if (total <= 400){
             let price = 0
             return price
-        }
-        if (total > 400 && total <= 200)
+        } 
+        else if (total > 400 && total <= 2000){
             let cost = charge * (total - 400)
             return cost
+        }
+        else if (total >= 2000 && total <= 2500){
+            return 25
+        }
+        else {
+            let greeting = 'cannot travel that far'
+            return greeting
+        }
+
+
             //return cost = (total - 400) * charge
     }
